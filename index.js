1d2cfd9ca3ce9288d9835ce797c9a342d57dd842
christina makatura <cnmak@comcast.net>
2021-08-15 07:02:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+    let num1 = 31
+    let num2 = 2
+    const multiply = (num1 * num2);
+
+    const random = Math.floor(Math.random()+1);
+
+    let num3 = 16
+    let num4 = 6
+    const mod = num3 %= num4;
+
+    let num5 = 10
+    let num6 = 15
+    let num7 = 20
+    const max = Math.max(num5, num6, num7)
 // Write your code here
